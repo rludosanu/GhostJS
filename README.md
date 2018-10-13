1d4a428862ade2956666ea53d0e5286f45ab4f4e
Razvan Ludosanu <rludosanu@Razvans-iMac.local>
2018-10-13 19:30:49 +0200

    Path matching RegExp added, README updated

--- README.md
+++ README.md
@@ -1,3 +1,144 @@
 # GhostJS
 
 A minimalist web framework for Node.js inpired by Express.
+
+## Hello World !
+
+Create a `server.js` file that will be hosting the base code of your GhostJS instance.
+
+```
+// Define the port the server will be running on
+const port = 3000;
+
+// Create a server instance
+const server = require('./ghost-server')(port);
+
+// Define your index root
+server.router([{
+	method: 'GET',
+	path: '/',
+	handler: (req, res) => {
+		res.render('/');
+	}
+});
+
+// Start the server
+server.start();
+```
+
+In the same directory create a `index.html` template file.
+
+```
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello World !</title>
+  </head>
+  <body>
+    <h1>Hello World !</h1>
+  </body>
+</html>
+```
+
+Run the server from your console.
+
+```
+$ node server.js
+```
+
+## Basic routing
+
+Routing refers to determining how an application responds to a client request to a particular endpoint, which is a URI and a specific HTTP request method.
+
+Each route can have one or more handler functions, which are executed when the route is matched.
+
+Route definition takes the following generic structure:
+
+```
+{
+	method: '[GET|POST|PUT|DELETE]',
+	path: '/[*]',
+	handler: (req, res) => {
+		res.render('/[*]');
+	}
+}
+```
+
+To apply the previous definition, just call the `router` function like so:
+
+```
+// Define route
+const route = {
+	method: 'GET',
+	path: '/hello',
+	handler: (req, res) => {
+		res.render('/hello.html');
+	}
+}
+
+// Save route
+server.router([route]);
+```
+
+## Advanced routing
+
+### Anonymous query parameters
+
+The `path` property of the route object is actually interpreted by the server as a RegExp.
+
+For example, the following path `/user/profile/([a-zA-Z0-9]+)` could match `/user/profile/rludosanu` or `/user/profile/razvan` and so on.
+
+The matched expression(s) are stored into an ordered array accessible via your handler parameter `request` at `request.query`.
+
+```
+{
+  ...
+  path: '/user/profile/([a-zA-Z0-9]+)',
+  handler: (request, response) => {
+    // This will print out the variable captured by the parenthesis in the RegExp
+    console.log(request.query[0]);
+  }
+}
+```
+
+### Named query parameters
+
+If you wish to explicitly name you parameters, it's also possible using an extra property named `query`.
+
+```
+{
+  ...
+  path: '/user/profile/([a-zA-Z0-9]+)',
+  query: ['username'],
+  handler: (request, response) => {
+    // This will print out the variable captured by the parenthesis in the RegExp
+    console.log(request.query.username);
+  }
+}
+```
+
+Note that the order of declaration matters and is read from left to right.
+
+For example, if you call the URI `http://localhost/user/profile/rludosanu/Paris` with this configuration:
+
+```
+{
+  ...
+  path: '/user/profile/([a-zA-Z0-9]+)/([a-zA-Z0-9]+)',
+  query: ['username', 'city'],
+  handler: (request, response) => {
+    console.log(request.query.username); // Prints out "rludosanu"
+    console.log(request.query.city); // Prints out "Paris"
+  }
+}
+```
+
+## Virtual paths
+
+To create a virtual path prefix (where the path does not actually exist in the file system) for files that are served , specify a mount path for the static directory, as shown below:
+
+```
+server.use('/css', 'src/stylesheets');
+```
+
+This means when you request the file `/css/bootstrap.css` the server will actually fetch the file located at `src/stylesheets/bootstrap.css` on your disk.
---
+++ ghost-server.js
@@ -0,0 +1,172 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+const mustache = require('mustache');
+
+class GhostServer {
+	constructor(port) {
+		if (typeof port === 'undefined' || parseInt(port) === 'NaN' || parseInt(port) <= 0) {
+			this.port = 8080;
+		} else {
+			this.port = parseInt(port);
+		}
+		this.staticRoutes = {};
+		this.routes = {};
+		this.server = null;
+		this.mimeTypes = {
+		  '.ico': 'image/x-icon',
+		  '.html': 'text/html',
+		  '.js': 'text/javascript',
+		  '.json': 'application/json',
+		  '.css': 'text/css',
+		  '.png': 'image/png',
+		  '.jpg': 'image/jpeg',
+		  '.wav': 'audio/wav',
+		  '.mp3': 'audio/mpeg',
+		  '.svg': 'image/svg+xml',
+		  '.pdf': 'application/pdf',
+		  '.doc': 'application/msword',
+		  '.eot': 'appliaction/vnd.ms-fontobject',
+		  '.ttf': 'aplication/font-sfnt'
+		};
+	}
+
+	router(routes) {
+		let self = this;
+
+		for (let i = 0 ; i < routes.length ; i++) {
+			let { method, path, handler } = routes[i];
+
+			if (!method || !path || !handler) {
+				continue;
+			}
+
+			if (!Object.keys(self.routes).includes(path)) {
+				self.routes[path] = {};
+			}
+
+			self.routes[path][method] = {
+				handler: handler,
+				query: (routes[i].hasOwnProperty('query')) ? routes[i].query : null
+			};
+			console.log(`Adding new route handler: ${method} ${path}`);
+		}
+	}
+
+	render(server, template, datas = null) {
+		let self = this;
+		let directoryname = path.dirname(template);
+		let filename = path.basename(template);
+		let pathname = null;
+		let extension = null;
+
+		// URL rewriting
+		if (Object.keys(server.staticRoutes).includes(directoryname)) {
+			pathname = path.join(__dirname, server.staticRoutes[directoryname], filename);
+		} else {
+			pathname = path.join(__dirname, template);
+		}
+		console.log(`Fetching file: ${pathname}`);
+
+		fs.exists(pathname, function (exist) {
+			if (!exist) {
+				self.statusCode = 404;
+				self.end(`File '${filename}' not found!`);
+				return;
+			}
+
+			if (fs.statSync(pathname).isDirectory()) {
+				pathname += '/index.html';
+			}
+
+			extension = path.extname(pathname);
+
+			fs.readFile(pathname, function(error, content) {
+				if (error) {
+					self.statusCode = 500;
+					self.end(`Error getting the file '${filename}': ${error}.`);
+				} else {
+					self.setHeader('Content-type', server.mimeTypes[extension] || 'text/plain');
+					if (extension === '.html' && datas !== null && Object.keys(datas).length !== 0) {
+						self.end(mustache.render(content.toString(), datas));
+					} else {
+						self.end(content);
+					}
+				}
+			});
+		});
+	}
+
+	use(path, directory) {
+		this.staticRoutes[path] = directory;
+		console.log(`Adding new directory alias: ${path} => ${directory}`)
+	}
+
+	parseRequest(request) {
+		let self = this;
+		let keys = Object.keys(self.routes);
+
+		for (let i = 0 ; i < keys.length ; i++) {
+			let key = keys[i];
+			let path = RegExp(`^${key}$`);
+
+			if (path.test(request.url)) {
+				let query = request.url.match(path).reduce((a, c) => typeof c === 'string' ? a.concat([c]) : a, []).slice(1);
+				let callback = self.routes[key][request.method].handler;
+
+				if (callback) {
+					query = self.routes[key][request.method].query.reduce((a, c, i) => {
+					  a[c] = query[i];
+					  return a;
+					}, []);
+				}
+
+				return {
+					query: query,
+					callback: callback
+				}
+			}
+		}
+
+		return {
+			query: null,
+			callback: null
+		};
+	}
+
+	handleRequest(request, response) {
+		let self = this;
+		let { query, callback } = self.parseRequest(request);
+
+		request.query = query;
+		response.render = self.render.bind(response, self);
+
+		if (callback) {
+			callback(request, response);
+		} else {
+			self.render.bind(response, self)(request.url);
+		}
+	}
+
+	start() {
+		let self = this;
+
+		console.log('Creating HTTP server');
+		self.server = http.createServer((req, res) => {
+			console.log(`Handling request: ${req.method} ${req.url}`);
+			self.handleRequest(req, res);
+		});
+
+		self.server.listen(self.port, (error) => {
+			if (error) {
+				return console.error(error);
+			}
+			console.log(`HTTP server is up and running on port ${self.port}`);
+		});
+	}
+}
+
+module.exports = function(options) {
+	return new GhostServer(options);
+}
